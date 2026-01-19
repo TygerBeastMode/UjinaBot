a57db4139c1f5edd49ab7e7a98c3a864dd89757a
tycespicee <166870260+TygerBeastMode@users.noreply.github.com>
2026-01-19 15:10:09 -0600

    Update roblox_stats.py

--- roblox_stats.py
+++ roblox_stats.py
@@ -7,8 +7,9 @@ async def get_group_info(group_id: int):
             if resp.status != 200:
                 return None, None
             data = await resp.json()
-            return data["name"], data["memberCount"]
-
+            name = data.get("name")
+            members = data.get("memberCount", 0)
+            return name, members
 
 async def get_game_stats(universe_id: int):
     url = f"https://games.roblox.com/v1/games?universeIds={universe_id}"
@@ -16,5 +17,12 @@ async def get_game_stats(universe_id: int):
         async with session.get(url) as resp:
             if resp.status != 200:
                 return None, None, None
-            data = (await resp.json())["data"][0]
-            return data["name"], data["visits"], data["likes"]
+            json_data = await resp.json()
+            game_data_list = json_data.get("data", [])
+            if not game_data_list:
+                return None, None, None
+            data = game_data_list[0]
+            name = data.get("name")
+            visits = data.get("visits", 0)
+            likes = data.get("likes") or data.get("favoritedCount") or 0
+            return name, visits, likes
